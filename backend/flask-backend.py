5c47598236144039f447910c26e9c50d8497e49d
Kehinde Adeoso <kennolite@gmail.com>
2024-09-29 00:28:04 -0400

    Amended code

--- backend/flask-backend.py
+++ backend/flask-backend.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify
 import pickle
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 # Initializing the Flask application
 app = Flask(__name__)
+CORS(app)
 
 # Importing ML model
 with open('elastic_net_model.pkl', 'rb') as file:
